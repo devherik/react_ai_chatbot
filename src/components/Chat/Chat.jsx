ce10cc4e5297617bb07feeac446cbc5cf59d7aa6
Herik Colares Rezende <dev.herik@gmail.com>
2025-05-04 17:42:58 -0300

    add scroll to end

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,19 +1,27 @@
-import Markdown from 'react-markdown';
-import styles from './Chat.module.css';
+import { useEffect, useRef } from "react";
+import Markdown from "react-markdown";
+import styles from "./Chat.module.css";
 
 const WELCOME_MESSAGE = {
-    role: 'assistant',
-    content: 'Hello, how can I help you today?',
-}
+  role: "assistant",
+  content: "Hello, how can I help you today?",
+};
 
 export function Chat({ messages }) {
-    return (
-        <div className={styles.Chat}>
-            {[WELCOME_MESSAGE, ...messages].map(({ role, content }, index) => (
-                <div key={index} className={styles.Message} data-role={role}>
-                    <Markdown>{content}</Markdown>
-                </div>
-            ))}
+  const messageEndRef = useRef(null);
+
+  useEffect(() => {
+    messageEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  return (
+    <div className={styles.Chat}>
+      {[WELCOME_MESSAGE, ...messages].map(({ role, content }, index) => (
+        <div key={index} className={styles.Message} data-role={role}>
+          <Markdown>{content}</Markdown>
         </div>
-    );
+      ))}
+      <div ref={messageEndRef} />
+    </div>
+  );
 }
