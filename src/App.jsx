46b30196b8ae10ba84f3d5bdffec561afaf04aa7
Herik Colares Rezende <dev.herik@gmail.com>
2025-05-04 16:37:11 -0300

    add a loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Assintant } from "./assistants/googleai.js";
+import { Loader } from "./components/Loader/Loader.jsx";
 import { Chat } from "./components/Chat/Chat.jsx";
 import { Controls } from "./components/Controls/Controls.jsx";
 import styles from "./App.module.css";
@@ -7,6 +8,7 @@ import styles from "./App.module.css";
 function App() {
   const assistant = new Assintant();
   const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   function addMessage(message) {
     setMessages((prevMessages) => [...prevMessages, message]);
@@ -14,6 +16,7 @@ function App() {
 
   async function handleContentSend(content) {
     addMessage({ content, role: "user" });
+    setLoading(true);
     try {
       const result = await assistant.chat(content);
       addMessage({ content: result, role: "assistant" });
@@ -22,11 +25,14 @@ function App() {
         content: "Sorry, I couldnt process your request. Please try again.",
         role: "system",
       });
+    } finally {
+      setLoading(false);
     }
   }
 
   return (
     <div className={styles.App}>
+      {loading && <Loader />}
       <header className={styles.Header}>
         <img className={styles.Logo} src="/chat-bot.png" />
         <h2 className={styles.Title}>AI ChatBot</h2>
