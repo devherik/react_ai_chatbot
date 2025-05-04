370e092812fb07013656b6dc360eff6de10a665f
Herik Colares Rezende <dev.herik@gmail.com>
2025-05-04 16:55:27 -0300

    some visual improves

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -1,43 +1,52 @@
-import { useState } from 'react';
-import styles from './Controls.module.css';
+import { useState } from "react";
+import TextAreaAutosize from "react-textarea-autosize";
 
-export function Controls({ onSend }) {
-    const [content, setContent] = useState('');
+import styles from "./Controls.module.css";
 
-    function handleContentChange(event) {
-        setContent(event.target.value);
+export function Controls({ isDisabled = false, onSend }) {
+  const [content, setContent] = useState("");
+
+  function handleContentChange(event) {
+    setContent(event.target.value);
+  }
+
+  function handleContentSend() {
+    if (content.length > 0) {
+      onSend(content);
+      setContent("");
     }
+  }
 
-    function handleContentSend() {
-        if (content.length > 0) {
-            onSend(content);
-            setContent('');
-        }
+  function handleEnterPress(event) {
+    if (event.key === "Enter" && !event.shiftKey) {
+      handleContentSend();
     }
+  }
 
-    function handleEnterPress(event) {
-        if (event.key === 'Enter' && !event.shiftKey) {
-            handleContentSend();
-        }
-    }
-
-    return (
-        <div className={styles.Controls}>
-            <div className={styles.TextAreaContainer}>
-                <textarea
-                    className={styles.TextArea}
-                    placeholder="Message AI ChatBot"
-                    value={content}
-                    onChange={handleContentChange}
-                    onKeyDown={handleEnterPress}
-                />
-            </div>
-            <button className={styles.Button} onClick={handleContentSend}><SendIcon/></button>
-        </div>
-    );
-    function SendIcon(params) {
-        return (
-            <img src="/send.png" className={styles.SendIcon}></img>
-        );
-    }
+  return (
+    <div className={styles.Controls}>
+      <div className={styles.TextAreaContainer}>
+        <TextAreaAutosize
+          className={styles.TextArea}
+          disabled={isDisabled}
+          placeholder="Message AI ChatBot"
+          value={content}
+          minRows={1}
+          maxRows={4}
+          onChange={handleContentChange}
+          onKeyDown={handleEnterPress}
+        />
+      </div>
+      <button
+        disabled={isDisabled}
+        className={styles.Button}
+        onClick={handleContentSend}
+      >
+        <SendIcon />
+      </button>
+    </div>
+  );
+  function SendIcon(params) {
+    return <img src="/send.png" className={styles.SendIcon}></img>;
+  }
 }
